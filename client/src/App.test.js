1fef0e069c4363e9a0757bd7f763119e9375b7b2
Ferdy van den Berg <fer@ghozt.nl>
2020-11-18 08:29:39 +0000

    Update test suite

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,4 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  
-});
+test('Renders learn react link', () => {});
